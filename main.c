4d5601312164eefc919c08aa315d1ad3f67f91c9
Denis <ddd@ddd.com>
2023-04-15 11:42:07 +0300

    2023...

--- main.c
+++ main.c
@@ -1,37 +1,58 @@
 #include <iostream>
 #include <fstream>
+#include <cassert>
 
-//#define TEST
-//#undef TEST
+void test_function(void (*test)(), const std::string& nameFunc) {
+	std::cout << "test func" << std::endl;//"\x1b[41mstart \x1b[32mtest\x1b[40m" << std::endl;
+	std::cout << "\t" << nameFunc << std::endl;
+	test();
+}
 
-#define A(n) (std::cout << #n << " - " << n  << std::endl)
-#define SetBit(numBit)	(1 << numBit)
+#define RUN_TEST(f)	(test_function(f, #f))
 
-#define Mult(a, b) ((a) * (b))
+template<class T>
+void assert_equal(const T expr, const T expr_true, 
+					const std::string& str_expr, const std::string& str_expr_true)
+{
+	if (expr == expr_true) {
+		std::cout << "\tTest PASSED" << std::endl;
+		return;
+	}
 
-struct Data {
-	int k;
-	int k1;
-	Data() : k(100), k1(500) {}
-};
+	std::cout << "\tTest BAD : " << str_expr << " != " << str_expr_true << std::endl;
+}
 
-std::ostream& operator<<(std::ostream& os, const Data& t) {
-	os << "{" << t.k << "," << t.k1 << "}";
-	return os;
+#define ASSERT_EQUAL(expr, expr_true) assert_equal(expr, expr_true, #expr, #expr_true)
+
+void assert1(bool expr, const std::string& str_expr) {
+	if (expr == true) {
+		std::cout << "\tTest OK" << std::endl;
+		return ;
+	}
+	
+	std::cout << "\tTest BAD : " << __func__ << " " << __LINE__ << " " << __FILE__ << " "  << str_expr << std::endl;
+}
+
+#define ASSERT(expr)	assert1(expr, #expr)	
+#define ASSERT1(expr)	std::cout << __FILE__ << ":" << __LINE__;\
+						 assert1(expr, #expr)	
+
+int mult(int a, int b) {
+	return a * b;
+}
+
+void test1() {
+	ASSERT_EQUAL(mult(2,2), 4);
+	ASSERT_EQUAL(mult(11,11), 121);
+	ASSERT_EQUAL(mult(11,11), 121);
+	ASSERT_EQUAL(mult(12,12), 144);
+	ASSERT1(mult(2,2) == 3);
+	ASSERT1(mult(2,2) == 5);
+	std::cout << "OK" << std::endl;
 }
 
 int main() {
-	std::cout << std::hex << SetBit(31) << " " << SetBit(12) << std::endl;
-	std::cout << std::dec << Mult(10,10) << " " << Mult(10 + 10, 2) << std::endl;
-
-	A(100);
-	int k = 101;
-	A(k);
-
-//	std::cout << Data() << std::endl;
-
-	Data temp;
-	A(temp);
+	RUN_TEST(test1);
 
 	return 0;
 }
